597e4b3f011ecd61730132208bfa1b548171436a
Luke Wilmoth <48221704+lawilmoth@users.noreply.github.com>
2025-11-18 09:08:25 -0500

    Clean up triangle functions and improve comments
    Removed unused triangle functions and added comments for clarity.

--- assignment.py
+++ assignment.py
@@ -1,109 +1,26 @@
 # Think like a Programmer - Chapter 2 Assignments
 
-def square(n):
-    """Create a function that makes a square of size n
-    Ex: square(2)
-    output:
-    ##
-    ##
-     
-    Ex square(5)
-    output:
-    #####
-    #####
-    #####
-    #####
-    #####
-    """
-
-    for i in range(n):
-        print('#' * n)
-
-def triange1(n):
-    """Create a function that makes a right triangle of size n
-    Ex: triange1(3)
-    output:
-    #
-    ##
-    ###
-     
-    Ex triange1(5)
-    output:
-    #
-    ##
-    ###
-    ####
-    #####
-    """
-
-    pass
-
-def triangle2(n):
-    """Create a function that makes an upside down right triangle of size n
-    Ex: triangle2(3)
-    output:
-    ###
-    ##
-    #
-     
-    Ex triangle2(5)
-    output:
-    #####
-    ####
-    ###
-    ##
-    #
-    """
-
-    pass
-
-def triangle3(n):
-    """Create a function that makes a centered triangle of size n
-    Ex: triangle3(3)
-    output:
-      #
-     ###
-    #####
-     
-    Ex triangle3(5)
-    output:
-        #
-       ###
-      #####
-     #######
-    #########
-    """
-
-    pass
-
-
 def find_first_digit(n):
     """Create a function that returns the first digit found in an integer n
     Ex: find_first_digit(12345) -> 1
-    Ex: find_first_digit(9081726354) -> 9"""
+    Ex: find_first_digit(9081726354) -> 9
+    
+    return the first digit as an integer
+    return None if n has no digits"""
     pass
 
 def loop_over_all_digits(n):
     """Create a function that loops over all digits in an integer n and prints each digit
     Ex: loop_over_all_digits(123)
+    append all digits to a list and return the list
     output:
-    1
-    2
-    3
+    [1, 2, 3]
 
     Ex: loop_over_all_digits(9081726354)
     output:
-    9
-    0
-    8
-    1
-    7
-    2
-    6
-    3
-    5
-    4
+    [9, 0, 8, 1, 7, 2, 6, 3, 5, 4]
     """
+    all_digits = []
     pass
 
 def luhn_check(n):
@@ -111,118 +28,6 @@ def luhn_check(n):
     Ex: luhn_check(49927398716) -> True
     Ex: luhn_check(1234567812345670) -> True
     Ex: luhn_check(1234567812345678) -> False
-    """
-    pass
-
-def square(n):
-    """Create a function that makes a square of size n
-    Ex: square(2)
-    output:
-    ##
-    ##
-     
-    Ex square(5)
-    output:
-    #####
-    #####
-    #####
-    #####
-    #####
-    """
-
-    pass
-
-def triange1(n):
-    """Create a function that makes a right triangle of size n
-    Ex: triange1(3)
-    output:
-    #
-    ##
-    ###
-     
-    Ex triange1(5)
-    output:
-    #
-    ##
-    ###
-    ####
-    #####
-    """
-
-    pass
-
-def triangle2(n):
-    """Create a function that makes an upside down right triangle of size n
-    Ex: triangle2(3)
-    output:
-    ###
-    ##
-    #
-     
-    Ex triangle2(5)
-    output:
-    #####
-    ####
-    ###
-    ##
-    #
-    """
-
-    pass
-
-def triangle3(n):
-    """Create a function that makes a centered triangle of size n
-    Ex: triangle3(3)
-    output:
-      #
-     ###
-    #####
-     
-    Ex triangle3(5)
-    output:
-        #
-       ###
-      #####
-     #######
-    #########
-    """
-
-    pass
-
-
-def find_first_digit(n):
-    """Create a function that returns the first digit found in an integer n
-    Ex: find_first_digit(12345) -> 1
-    Ex: find_first_digit(9081726354) -> 9"""
-    pass
-
-def loop_over_all_digits(n):
-    """Create a function that loops over all digits in an integer n and prints each digit
-    Ex: loop_over_all_digits(123)
-    output:
-    1
-    2
-    3
-
-    Ex: loop_over_all_digits(9081726354)
-    output:
-    9
-    0
-    8
-    1
-    7
-    2
-    6
-    3
-    5
-    4
-    """
-    pass
-
-def luhn_check(n):
-    """Create a function that checks if the integer n passes the Luhn check
-    Ex: luhn_check(49927398716) -> True
-    Ex: luhn_check(1234567812345670) -> True
-    Ex: luhn_check(1234567812345678) -> False
+    return True if n passes the Luhn check, False otherwise
     """
     pass
